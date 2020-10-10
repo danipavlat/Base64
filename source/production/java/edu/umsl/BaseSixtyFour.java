33b6730fcd297542e14592f67419baa64be3babe
Dani Pavlat <dpnqv@umsystem.edu>
2020-10-09 23:35:52 -0500

    Initial commit.

---
+++ source/production/java/edu/umsl/BaseSixtyFour.java
@@ -0,0 +1,8 @@
+package edu.umsl;
+import org.apache.commons.codec.binary.Base64;
+
+public class BaseSixtyFour {
+    public static void main (String[] args){
+        System.out.println("Initial commit.");
+    }
+}
